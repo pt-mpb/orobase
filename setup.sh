9c8590d5db798ce067cf4b708bfa50e8ed308074
Mario Bessa <mario.bessa@skrey-software.com>
2019-04-12 11:46:39 +0100

    Yet another fix...

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ apt-get install -qqy apt-transport-https ca-certificates vim nano make git-core 
 mysql-client zip unzip redis-tools netcat-openbsd
 
 # Install php
-apt-get install -qqy --no-install-recommends php7.1-fpm php7.1-cli php7.1-common php7.1-dev \
+apt-get install -qqy php7.1-fpm php7.1-cli php7.1-common php7.1-dev \
 php7.1-mysql php7.1-curl php7.1-gd php7.1-mcrypt php7.1-xmlrpc php7.1-ldap \
 php7.1-xsl php7.1-intl php7.1-soap php7.1-mbstring php7.1-zip php7.1-bz2 php7.1-redis php7.1-tidy || exit 1
 
